1115054f37756cc0d7e5c95e1f84dd67662cd9fd
emaslov <emaslov@tradingview.com>
2022-08-05 03:04:46 +0400

    lol

--- json.hpp
+++ json.hpp
@@ -20,20 +20,19 @@ public:
     virtual ~IJSON() {}
 };
 
-template <typename T>
+template <template <typename> class T, typename ParentType>
 class JSONBase
-    : public std::enable_shared_from_this<T>
+    : public std::enable_shared_from_this<T<ParentType>>
     , public IJSON
 {
-public:
-    JSONBase(JSONPtr parent)
+protected:
+    JSONBase(std::shared_ptr<ParentType> parent)
         : m_parent(parent)
     {
-
     }
 
 protected:
-    std::weak_ptr<JSONBase<T>> m_parent;
+    std::weak_ptr<ParentType> m_parent;
 };
 
 using JSONTypes = TypesPacker<int64_t, double, std::string, JSONPtr, std::monostate>;
@@ -58,37 +57,37 @@ concept IsJSONAdders = requires(T o) {
 template <typename T>
 class JSONAdders
 {
-    auto& type()
+    auto type()
     {
         static_assert(IsJSONAdders<T>);
         return *static_cast<T*>(this);
     }
 
-    auto& returnValue()
+    auto returnValue()
     {
-        return type().returnValue();
+        return type().reset();
     }
 
 public:
-    auto& integer(int64_t val)
+    auto integer(int64_t val)
     {
         type().addValue(val);
         return returnValue();
     }
 
-    auto& real(double val)
+    auto real(double val)
     {
         type().addValue(val);
         return returnValue();
     }
 
-    auto& string(const std::string& val)
+    auto string(const std::string& val)
     {
         type().addValue(val);
         return returnValue();
     }
 
-    auto& null()
+    auto null()
     {
         type().addValue(std::monostate());
         return returnValue();
@@ -97,84 +96,74 @@ public:
 
 template <typename T>
 class JSONKey
-    : public JSONBase<JSONKey<T>>
+    : public JSONBase<JSONKey, T>
     , public JSONAdders<JSONKey<T>>
-    , public std::enable_shared_from_this<JSONKey<T>>
 {
+    using BaseType = JSONBase<JSONKey, T>;
+    using BaseType::shared_from_this;
     using ParentPtr = std::shared_ptr<T>;
     using JSONKeyPtr = std::shared_ptr<JSONKey<T>>;
-    using std::enable_shared_from_this<JSONKey<T>>::shared_from_this;
 public:
     static JSONKeyPtr create(ParentPtr parent)
     {
-        return std::make_shared<JSONKey<T>>(parent)->shared_from_this();
+        return JSONKeyPtr(new JSONKey(parent));
     }
 
     void addValue(JSONValue value)
     {
-
+        m_value = value;
     }
 
-    std::shared_ptr<JSONObject<T>> beginObject()
-    {
-
-    }
+    std::shared_ptr<JSONObject<JSONKey<T>>> beginObject();
 
     std::shared_ptr<T> reset()
     {
-        static_cast<T*>(JSONBase<JSONKey<T>>::m_parent.lock().get())->enable_shared_from_this();
+        return BaseType::m_parent.lock();
     }
 
 private:
-    JSONKey(JSONPtr parent)
-        : JSONBase<JSONKey<T>>(parent)
+    JSONKey(ParentPtr parent)
+        : BaseType(parent)
     {}
+
+private:
+    std::string m_key;
+    JSONValue m_value;
 };
 
 template <typename T>
 class JSONObject
-    : public JSONBase<JSONObject<T>>
-    , public std::enable_shared_from_this<JSONObject<T>>
+    : public JSONBase<JSONObject, T>
 {
+    using BaseType = JSONBase<JSONObject, T>;
+    using BaseType::shared_from_this;
     using ParentPtr = std::shared_ptr<T>;
-    using JSONObjectPtr = std::shared_ptr<JSONObject>;
-    using std::enable_shared_from_this<JSONObject<T>>::shared_from_this;
+    using JSONObjectPtr = std::shared_ptr<JSONObject<T>>;
 public:
-    static JSONObjectPtr create(ParentPtr parent = nullptr)
+    static JSONObjectPtr create(ParentPtr parent)
     {
-        return (new JSONObject<T>(parent))->shared_from_this();
+        return JSONObjectPtr(new JSONObject(parent));
     }
 
     std::shared_ptr<JSONKey<JSONObject<T>>> key(const std::string& key)
     {
-        auto obj = (new JSONKey<JSONObject<T>>())->shared_from_this();
-        auto [iter, ok] =
-            m_fields.emplace(key, obj);
+        auto obj = JSONKey<JSONObject<T>>::create(shared_from_this());
+        auto [_, ok] = m_fields.emplace(key, obj);
         if (!ok)
         {
             throw std::runtime_error("Cannot create key!");
         }
-        return *iter;
+        return obj;
     }
 
-//    JSONObjectPtr beginObject(const std::string& key)
-//    {
-//        auto [item, ok] = m_fields.emplace(
-//            std::pair<std::string, JSONObjectPtr>{key, create()});
-
-//        if (!ok) throw std::runtime_error("Failed to begin object!");
-
-//        return std::get<JSONObjectPtr>(item->second)->ptr();
-//    }
-
-    T& endObject()
+    std::shared_ptr<T> endObject()
     {
-        return *static_cast<T*>(m_parent.lock()->ptr());
+        return BaseType::m_parent.lock()->ptr();
     }
 
 private:
-    JSONObject(JSONPtr parent)
-        : JSONBase<JSONObject<T>>(parent)
+    JSONObject(ParentPtr parent)
+        : BaseType(parent)
     {}
 
 private:
@@ -194,5 +183,28 @@ private:
     std::vector<JSONValue> m_values;
 };
 
+// JSONKey
+template <typename T>
+std::shared_ptr<JSONObject<JSONKey<T>>> JSONKey<T>::beginObject()
+{
+    auto ptr = JSONObject<JSONKey<T>>::create(shared_from_this());
+    m_value = ptr;
+    return ptr;
+}
+
+class JSONRoot
+{
+    using RootObject = JSONObject<JSONRoot>;
+    using RootObjectPtr = std::shared_ptr<RootObject>;
+public:
+    static RootObjectPtr create()
+    {
+        return RootObject::create(std::shared_ptr<JSONRoot>());
+    }
+
+private:
+    JSONRoot(){};
+    JSONRoot(std::shared_ptr<JSONRoot>){};
+};
 
 #endif //JSON_HPP
